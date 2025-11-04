7c040829fd72e3a6ab94524c212f311eb5c05885
manojrai200 <manojrai9801@gmail.com>
2025-11-04 14:05:45 +0545

    13.3

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-require('dotenv').config()
-
-const { Sequelize, QueryTypes } = require('sequelize')
+require("dotenv").config();
+const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+    ssl: { require: true, rejectUnauthorized: false },
   },
 });
 
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
 const main = async () => {
   try {
-    await sequelize.authenticate()
-
+    await sequelize.authenticate();
     const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    blogs.map(blog => {
-        console.log(`${blog.author}: ${blog.url}, ${blog.likes} likes`)
-    })
-    sequelize.close()
+    blogs.forEach((blog) => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
+    });
+    await sequelize.close();
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    console.log('Unable to connect to the database:', error);
   }
-}
+};
 
-main()
+main();
