2faa48680b258c2e49221119e3808c9011b1f8f0
manojrai200 <manojrai9801@gmail.com>
2025-11-05 13:16:10 +0545

    refactor

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,22 +1,24 @@
 // utils/middleware.js
-const logger = require('./logger')
+const logger = require("./logger");
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
+  res.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorHandler = (error, req, res, next) => {
-  logger.error(error.message)
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'SequelizeValidationError') {
-      return res.status(400).json({ error: error.message })
-    } else if (error.name === 'SequelizeDatabaseError') {
-      return res.status(400).json({ error: error.message })
-    }
-  
-  next(error)
-}
+  logger.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "SequelizeValidationError") {
+    return res.status(400).json({ error: error.message });
+  } else if (error.name === "SequelizeDatabaseError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 module.exports = {
   unknownEndpoint,
