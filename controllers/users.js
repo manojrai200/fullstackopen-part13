a6e15890f920a0def8d49883fc2d69bedf95825b
manojrai200 <manojrai9801@gmail.com>
2025-11-05 13:38:31 +0545

    13.8

---
+++ controllers/users.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+
+const { User } = require("../models");
+
+router.get("/", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+router.post("/", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:username", async (req, res) => {
+  const user = await User.findOne({
+  where: {
+    username: req.params.username
+  }
+})
+  if (!user) {
+    return res.status(404).json({ error: 'blog not found' })
+  }
+  user.username = req.body.username
+  await user.save()
+  res.json(user)
+});
+
+module.exports = router;
