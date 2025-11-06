1c04f67e894b9a4fc89843c32749d63dac774f3d
manojrai200 <manojrai9801@gmail.com>
2025-11-06 12:52:44 +0545

    13.7

--- utils/db.js
+++ utils/db.js
@@ -1,25 +1,42 @@
-const Sequelize = require('sequelize')
-const { DATABASE_URL } = require('./config')
+const Sequelize = require("sequelize");
+const { DATABASE_URL } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   },
 });
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const connectToDatabase = async () => {
   try {
-    await sequelize.authenticate()
-    console.log('connected to the database')
+    await sequelize.authenticate();
+    await runMigrations();
+    console.log("connected to the database");
   } catch (err) {
-    console.log('failed to connect to the database')
-    return process.exit(1)
+    console.log("failed to connect to the database");
+    return process.exit(1);
   }
 
-  return null
-}
+  return null;
+};
 
-module.exports = { connectToDatabase, sequelize }
+module.exports = { connectToDatabase, sequelize };
