96d8b6567374b4dd0bbf8b32dd26b686982dce99
manojrai200 <manojrai9801@gmail.com>
2025-11-07 15:26:51 +0545

    13.19

--- models/index.js
+++ models/index.js
@@ -1,10 +1,14 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./readingList");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+User.belongsToMany(Blog, { through: ReadingList, as: "reading_list" });
+Blog.belongsToMany(User, { through: ReadingList, as: "reading_list" });
 
 module.exports = {
   Blog,
   User,
+  ReadingList,
 };
