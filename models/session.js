808f5f98315fc9fac683af08a33147b2a6e61326
manojrai200 <manojrai9801@gmail.com>
2025-11-12 15:57:50 +0545

    feat: added db schema for sesson

---
+++ models/session.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require("../utils/db");
+
+class Session extends Model {}
+
+Session.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "session",
+  }
+);
+
+module.exports = Session;
